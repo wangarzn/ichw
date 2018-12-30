4f54d2d27675dc24a6a5cc55e4bab226e0fb9192
wang  qichao <43436181+wangarzn@users.noreply.github.com>
2018-12-30 10:22:43 +0800

    Update wcount.py

--- pyassign4/wcount.py
+++ pyassign4/wcount.py
@@ -17,6 +17,11 @@ from urllib.request import urlopen
 def wcount(lines, topn):
     """count words from lines of text string, then sort by their counts
     in reverse order, output the topn (word count), each in one line.
+    变量 lines：读取的文件
+    变量 topn ：打印的单词数目
+    变量 strlines ：字符串形式的文本（所有的单词均转化成了小写）
+    变量 ls1  ：把字符串格式的文本切成列表
+    变量 ls2  ：把ls1中每个元素转化为元组，元组第二项为该字符的数目，然后进行去重
     """
     strlines = lines.decode()  # 将字节流转化为字符串
     for c in string.punctuation:
@@ -28,12 +33,17 @@ def wcount(lines, topn):
     # ls2中包含互不重复的元组，元祖第一项为单词名称，第二项为单词在整个文本中出现的次数
     ls2.sort(key=lambda x: x[1], reverse=True)
     length = len(ls2)
-    for i in ls2[:topn]:
-        print(i[0], "   ", i[1])
+    if topn > length:
+        for i in ls2:
+            print(i[0], "   ", i[1])   
+    else:
+        for i in ls2[:topn]:
+            print(i[0], "   ", i[1])
 
 
 if __name__ == '__main__':
-
+    """ sys.argv 通过命令提示行得到的列表，一定存在第一项，为文件名。
+    """
     if len(sys.argv) == 1:
         # 命令行仅给了一个参数，则无法找到网页，无法计数
         print('Usage: {} url [topn]'.format(sys.argv[0]))
@@ -48,13 +58,8 @@ if __name__ == '__main__':
     if len(sys.argv) == 2:
         # 命令行给了2个参数，给出仅给出十个结果
         wcount(text0, 10)
-
     if len(sys.argv) == 3:
-        # 命令行给了3个参数，
-        if int(sys.argv[2]) <= length:
-            wcount(text0, int(sys.argv[2]))
-        else:
-            wcount(text0, length)
-
+        # 命令行给了3个参数，        
+        wcount(text0, int(sys.argv[2]))
     if len(sys.argv) > 3:
         print("输入的参数多于应该输入的参数")
